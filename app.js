81471c9b7dacf26667764389a6d99fdacfa587d3
NirjharJoshi <njupkiet@gmail.com>
2023-03-09 10:46:58 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
-// const cors = require('cors');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +28,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, fornt-end natours.com
+// app.use(
+//   cors({
+//     origin: 'http://www.natours.com'
+//   })
+// );
+
+app.options('*', cors());
+// app.options('api/v1/tour/:id', cors());
+
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
